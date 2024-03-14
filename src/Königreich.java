09426f53cc653fa6747d9376f0bf8efedd988b6c
Peter Dillinger <peter.dillinger@srh.de>
2024-03-14 10:27:17 +0100

    Scheint so zu funktionieren

--- src/Königreich.java
+++ src/Königreich.java
@@ -8,14 +8,30 @@ public class Königreich {
             System.out.print("Welche Bevölkerungsgruppe (K, A, B, L): ");
             char bevölkerungsgruppe = eingabe.next().toUpperCase().charAt(0);
             if (bevölkerungsgruppe != 'K' && bevölkerungsgruppe != 'A'
-                && bevölkerungsgruppe != 'B' && bevölkerungsgruppe != 'L' ) break;
+                && bevölkerungsgruppe != 'B' && bevölkerungsgruppe != 'L') break;
 
+            eingabe.nextLine(); // flush
             System.out.print("Wie heißt der Einwohner: ");
             String name = eingabe.nextLine();
 
             System.out.print("Wie viel verdient der Einwohner (im Jahr): ");
             int einkommen = eingabe.nextInt();
 
+            Einwohner einwohner;
+            if (bevölkerungsgruppe == 'K') {
+                einwohner = new König(name, einkommen);
+            }
+            else if (bevölkerungsgruppe == 'A') {
+                einwohner = new Adel(name, einkommen);
+            }
+            else if (bevölkerungsgruppe == 'B') {
+                einwohner = new Bauer(name, einkommen);
+            }
+            else {
+                einwohner = new Leibeigen(name, einkommen);
+            }
+            System.out.println("Der Einwohner " + name + " muss " + einwohner.steuer() +
+                    " Taler Steuern zahlen.");
         }
 
         System.out.println("Vielen Dank und auf Wiedersehen!");
